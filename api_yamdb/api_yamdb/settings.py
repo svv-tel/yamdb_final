a935fae058fa3cd9baa612afc9b374c4f6f91b71
Владимир Самошкин <svv-tel@yandex.ru>
2022-08-01 13:30:50 +0300

    Запуск сайта v.19

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,11 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '51.250.103.79',
-    'localhost',
-    'oblomtest.ddns.net',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
